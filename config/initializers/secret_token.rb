ccc8d7426a2a9eb16bc2d546c4f96d75f8de9c61
Dollie Schombert <dollie@npnparents.org>
2011-07-19 17:42:09 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '8309719d6de84ebbd7e85cf85e6d681f175b13a9351c6fc7cd47fed657943f87c4c9d377a82d8ee9ee4cd8ddc61628b9e1e077702b2869f4d22ca918371493f5'
