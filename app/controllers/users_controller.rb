8fb0e930b2bb135bc8484ca2fb30e532166b6707
Dollie Schombert <dollie@npnparents.org>
2011-08-19 15:21:05 -0500

    done with sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,16 @@ class UsersController < ApplicationController
     @user = User.new
     @title = "Sign up"
   end
+ 
   def create
-      @user = User.new(params[:user])
-      if  @user.save
-          flash[:success] = "Welcome to the Sample App!"
-          redirect_to @user
-      else
-          @title = "Sign up"
-          render 'new'
-      end
-  end
+     @user = User.new(params[:user])
+     if @user.save
+       sign_in @user
+       redirect_to @user, :flash => { :success => "Welcome to the Sample App!" }
+     else
+       @title = "Sign up"
+       render 'new'
+     end
+   end
+ 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 SampleApp::Application.routes.draw do
+  get "sessions/new"
+
     resources :users
+    resources :sessions, :only => [:new, :create, :destroy]
     
     root :to => "pages#home"
 
