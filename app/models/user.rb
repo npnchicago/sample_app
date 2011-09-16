7f566ed07db260340342799ada84c162353e905f
Dollie Schombert <dollie@npnparents.org>
2011-09-16 17:33:14 -0500

    added user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessible :name, :email, :password, :password_confirmation
   
+  has_many :microposts, :dependent => :destroy
+  
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, :presence => true,  
             :length => { :maximum => 50 }
@@ -20,6 +22,10 @@ class User < ActiveRecord::Base
       encrypted_password == encrypt(submitted_password)
   end
 
+  def feed
+      Micropost.where("user_id = ?", id)
+  end
+  
   class << self
     def authenticate(email, submitted_password)
       user = find_by_email(email)
@@ -31,7 +37,7 @@ class User < ActiveRecord::Base
       (user && user.salt == cookie_salt) ? user : nil
     end
   end
-  
+
   private
   
     def encrypt_password
